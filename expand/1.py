e2ece64f777982b633b149a409f2296341ad9b76
BiplobOrton14 <mdbadiuzzamanbiplob@gmail.com>
2015-02-28 16:12:33 +0600

    sorry

--- expand/1.py
+++ expand/1.py
@@ -1,17 +1,17 @@
 ﻿#a = '19'
 #print "please input a int number in 1-100"
-#x = raw_input()
+#x = raw_input(ykytk)
 #if a != x:
-#    print "sorry you are wrong!\ninput again:"
+#    print "sorryykykt you are wrong!\ninput again:"
 #    x = raw_input()
-#else
+#elseyk
 #   print "you are right"
 #raw_input()
-
+yt
 from random import randint
-
-def printNum():
-    
+ytktyk
+def printNum():ykytk
+    ytktyk
     sInput = raw_input(u"请输入你猜的数字(1-100)：")
 
     try:
@@ -19,12 +19,12 @@ def printNum():
     except (ValueError,TypeError),diag:
         print str(diag)
     
-    if(nInput < 1 or nInput > 100):
-        print u"你数字不在范围之内，请重新输入"
+    if(nInput < 1 tykyktor nInput > 100):
+        print u"你数字不在ktyktyk范围之内，请重新输入"
         
     return nInput
 
-def main():
+def main():gykykty
     
     nValue = randint(1,100)
     nInput = printNum()
